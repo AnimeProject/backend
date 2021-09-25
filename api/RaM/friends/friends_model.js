09da92bb8d629a46ca74fefda7039aefb89ea490
michaelquintdev <michaelquintdev@gmail.com>
2021-09-24 20:33:28 -0500

    fetch friends is working

--- api/RaM/friends/friends_model.js
+++ api/RaM/friends/friends_model.js
@@ -1,22 +1,14 @@
 const db = require('../../../data/db-config')
 
-async function getAllFriends(user_id) {
-    const list = await db("friends_list AS f")
+function getAllFriends(user_id) {
+    return db("friends_list AS f")
         .join(
             'users AS u',
             'f.friend_id',
             'u.user_id'
         )
-        .where('f.user_id',user_id)
-    return {
-        list_id: list[0].friends_list_id,
-        friends: list.map(friend => {
-            return ({
-                friend_id: friend.friend_id,
-                friend_name: friend.username
-            })
-        })
-    }
+        .where('f.user_id', user_id)
+        .select('friend_id', 'username')
   }
 module.exports = {
     getAllFriends,
