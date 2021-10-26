d259c207370148c9823749c7f4b143e95da8861c
michaelquintdev <michaelquintdev@gmail.com>
2021-10-25 20:33:59 -0500

    Changed some seed data to match API quick

--- data/seeds/04-lists.js
+++ data/seeds/04-lists.js
@@ -2,13 +2,13 @@ exports.seed = function(knex) {
     return knex('lists').del()
       .then(function () {
         return knex('lists').insert([
-          {user_id: 1, anime_id: 322, completed: 1, rating: 9},
-          {user_id: 3, anime_id: 2, completed: 1, rating: 5},
-          {user_id: 4, anime_id: 42, completed: 0, rating: 3},
+          {user_id: 1, anime_id: 28851, completed: 1, rating: 10},
+          {user_id: 3, anime_id: 1, completed: 1, rating: 5},
+          {user_id: 4, anime_id: 5114, completed: 0, rating: 3},
           {user_id: 2, anime_id: 1, completed: 0, rating: 3},
-          {user_id: 1, anime_id: 42, completed: 0, rating: 3},
-          {user_id: 2, anime_id: 42, completed: 0, rating: 3},
-          {user_id: 4, anime_id: 3, completed: 0, rating: 3},
+          {user_id: 1, anime_id: 5114, completed: 0, rating: 3},
+          {user_id: 2, anime_id: 5114, completed: 0, rating: 3},
+          {user_id: 4, anime_id: 19, completed: 0, rating: 3},
         ]);
       });
   };
